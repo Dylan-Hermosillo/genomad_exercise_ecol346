12a4c738b77524cc406a72d193e86eb491d4efba
Andarcher <113861407+Dylan-Hermosillo@users.noreply.github.com>
2025-04-17 23:51:51 -0700

    Update run_genomad.sh
    updated time

--- run_genomad.sh
+++ run_genomad.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=standard
 #SBATCH --nodes=1
 #SBATCH --ntasks=2
-#SBATCH --time=2:00:00
+#SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=12
 #SBATCH --mem-per-cpu=5gb
 #SBATCH --array=0-1
